94f42e2122a289dfa337aecf0346a975ceb9fd49
slyofzero <sly.of.zero@gmail.com>
2024-07-07 16:28:58 +0530

    fixes

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -44,6 +44,14 @@ To configure your buybot go to @${BOT_USERNAME} and do /settings.`;
 
         try {
           new web3.PublicKey(token);
+        } catch (error) {
+          errorHandler(error);
+          await ctx.reply("The token address you passed was incorrect.");
+        }
+
+        let pairs: string[] = [];
+
+        try {
           const data = (
             await apiFetcher<TokenData>(
               `https://api.dexscreener.com/latest/dex/tokens/${token}`
@@ -51,7 +59,7 @@ To configure your buybot go to @${BOT_USERNAME} and do /settings.`;
           ).data;
 
           const firstPair = data?.pairs.at(0);
-          const pairs = data.pairs.map(({ pairAddress }) => pairAddress);
+          pairs = data.pairs.map(({ pairAddress }) => pairAddress);
 
           if (!data || !firstPair) {
             log(`No data found for ${token}`);
@@ -59,48 +67,48 @@ To configure your buybot go to @${BOT_USERNAME} and do /settings.`;
               `No data found for ${token}. Either the token address you passed is invalid, or dexscreener doesn't have data about this token`
             );
           }
-
-          const projectData = (
-            await getDocument<StoredGroup>({
-              collectionName: "project_groups",
-              queries: [["chatId", "==", String(chatId)]],
-            })
-          ).at(0);
-
-          if (projectData) {
-            const { admins } = projectData;
-            const newAdmins = !admins.includes(admin)
-              ? [...admins, admin]
-              : admins;
-            await ctx.reply(cleanUpBotMessage(text), {
-              parse_mode: "MarkdownV2",
-            });
-            await updateDocumentById<StoredGroup>({
-              updates: { token, pairs, admins: newAdmins },
-              collectionName: "project_groups",
-              id: projectData.id || "",
-            });
-          } else {
-            await ctx.reply(cleanUpBotMessage(text), {
-              parse_mode: "MarkdownV2",
-            });
-            const data: StoredGroup = {
-              chatId: String(chatId),
-              token,
-              pairs,
-              admins: [admin],
-            };
-            await addDocument<StoredGroup>({
-              data,
-              collectionName: "project_groups",
-            });
-          }
-
-          syncProjectGroups();
         } catch (error) {
           errorHandler(error);
-          await ctx.reply("The token address you passed was incorrect.");
+          await ctx.reply("The token address you pasted is not detected.");
         }
+
+        const projectData = (
+          await getDocument<StoredGroup>({
+            collectionName: "project_groups",
+            queries: [["chatId", "==", String(chatId)]],
+          })
+        ).at(0);
+
+        if (projectData) {
+          const { admins } = projectData;
+          const newAdmins = !admins.includes(admin)
+            ? [...admins, admin]
+            : admins;
+          await ctx.reply(cleanUpBotMessage(text), {
+            parse_mode: "MarkdownV2",
+          });
+          await updateDocumentById<StoredGroup>({
+            updates: { token, pairs, admins: newAdmins },
+            collectionName: "project_groups",
+            id: projectData.id || "",
+          });
+        } else {
+          await ctx.reply(cleanUpBotMessage(text), {
+            parse_mode: "MarkdownV2",
+          });
+          const data: StoredGroup = {
+            chatId: String(chatId),
+            token,
+            pairs,
+            admins: [admin],
+          };
+          await addDocument<StoredGroup>({
+            data,
+            collectionName: "project_groups",
+          });
+        }
+
+        syncProjectGroups();
       } else {
         text += `To start the buy, add \\@${BOT_USERNAME} as an admin \\(this allows the bot to send messages\\) and then do /start in the below format -\n/start _token address_.`;
         await ctx.reply(cleanUpBotMessage(text), { parse_mode: "MarkdownV2" });
