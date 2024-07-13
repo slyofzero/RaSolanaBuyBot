bc1b6e08056c946e5ef6333e8ac17f4c48bd0872
slyofzero <sly.of.zero@gmail.com>
2024-07-13 18:59:27 +0530

    .env fixes

--- env.d.ts
+++ env.d.ts
@@ -1,31 +1,15 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      RPC_ENDPOINT: string | undefined;
+      TRENDING_CHANNEL: string | undefined;
       BOT_TOKEN: string | undefined;
-      FIREBASE_KEY: string | undefined;
-      HTTP_CLIENT: string | undefined;
-      TON_API_KEY: string | undefined;
-      WSS_ENDPOINT: string | undefined;
-      BOT_USERNAME: string | undefined;
-      TRENDING_BOT_USERNAME: string | undefined;
-      TONCLIENT_ENDPOINT: string | undefined;
-      TONCLIENT_API_KEY: string | undefined;
-      EXPLORER_URL: string | undefined;
-      DEX_URL: string | undefined;
-      GECKO_API: string | undefined;
       TRENDING_TOKENS_API: string | undefined;
       TRENDING_AUTH_KEY: string | undefined;
-      TRENDING_CHANNEL_ID: string | undefined;
-      TRENDING_CHANNEL: string | undefined;
+      BOT_USERNAME: string | undefined;
       PORT: string | undefined;
-      COINGECKO_API_KEY: string | undefined;
-      MAIN_ADDRESS: string | undefined;
-      TRENDING_PRICES: string | undefined;
-      AD_PRICES: string | undefined;
-      RPC_ENDPOINT: string | undefined;
-      HELIS_API_KEY: string | undefined;
-      WEBHOOK_URL: string | undefined;
-      TRENDING_BOT_TOKENS: string | undefined;
+      ENCRYPTION_KEY: string | undefined;
+      FIREBASE_KEY: string | undefined;
     }
   }
 }
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,34 +1,20 @@
 import dotenv from "dotenv";
-dotenv.config();
+
+export const { NODE_ENV } = process.env;
+dotenv.config({
+  path: NODE_ENV === "development" ? ".env" : ".env.production",
+});
 
 export const {
   BOT_TOKEN,
-  FIREBASE_KEY,
-  HTTP_CLIENT,
-  WSS_ENDPOINT,
   BOT_USERNAME,
-  TONCLIENT_ENDPOINT,
-  EXPLORER_URL,
-  DEX_URL,
-  TONCLIENT_API_KEY,
-  GECKO_API,
-  TRENDING_TOKENS_API,
-  TRENDING_CHANNEL,
-  TRENDING_AUTH_KEY,
-  TRENDING_BOT_USERNAME,
-  TRENDING_CHANNEL_ID,
-  TON_API_KEY,
-  PORT,
-  COINGECKO_API_KEY,
-  MAIN_ADDRESS,
-  TRENDING_PRICES,
-  AD_PRICES,
-  TOKEN_DATA_URL,
   ENCRYPTION_KEY,
-  CHANNEL_ID,
+  FIREBASE_KEY,
+  PORT,
   RPC_ENDPOINT,
-  HELIS_API_KEY,
-  WEBHOOK_URL,
+  TRENDING_AUTH_KEY,
+  TRENDING_CHANNEL,
+  TRENDING_TOKENS_API,
 } = process.env;
 
 export const TRENDING_BOT_TOKENS: string[] = JSON.parse(
