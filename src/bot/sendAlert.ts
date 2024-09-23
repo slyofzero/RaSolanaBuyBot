82d75450fb52237d96183d129a03df94eecb034d
slyofzero <sly.of.zero@gmail.com>
2024-09-23 20:33:08 +0530

    fixes

--- src/bot/sendAlert.ts
+++ src/bot/sendAlert.ts
@@ -68,7 +68,7 @@ export async function sendAlert(data: BuyData) {
     // links
     const buyerLink = `https://solscan.io/account/${buyer}`;
     const txnLink = `https://solscan.io/tx/${signature}`;
-    const dexSLink = `https://dexscreener.com/solana/7fdjh3zyup8ri6j8nglcpcxqsak8d9vbpab7pvibg4d1/${token}`;
+    const dexSLink = `https://dexscreener.com/solana/${token}`;
     const trendingLink = `https://t.me/c/2125443386/2`;
     const photonLink = `https://photon-sol.tinyastro.io/en/lp/${token}`;
     const trendingRank = Object.entries(trendingTokens).findIndex(
